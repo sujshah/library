0e765993456b48d2b7d56cfd49b323446ce55c8c
Suraj Shah <suraj1.shah@gmail.com>
2022-11-09 21:41:40 +0000

    Add right and left grid button functionality

--- constructors.js
+++ constructors.js
@@ -1,5 +1,5 @@
-let myLibrary = {};
-let count = 0
+let myLibrary = [];
+let boardNumber = 0
 
 function Book(title, author, pages, read) {
     this.title = title
@@ -13,24 +13,35 @@ function Book(title, author, pages, read) {
 }
 
 function addBookToLibrary(book) {
-    myLibrary[count] = book;
+    myLibrary[myLibrary.length] = book;
+    generateLibraryGridForBoardNum(boardNumber);
+}
+
+
+function generateLibraryGridForBoardNum(gridNum) {
     let libraryGrid = document.querySelector(".library-grid");
-    let div = document.createElement('div');
-    div.classList.add('book');
-    div.setAttribute("data-key", count.toString());
-    div.textContent = book.info();
-    let deleteButton = document.createElement('button');
-    deleteButton.textContent = "Delete";
-    deleteButton.setAttribute("type", "submit");
-    deleteButton.classList.add('delete');
-    let readButton = document.createElement('button');
-    readButton.textContent = "Read?";
-    readButton.setAttribute("type", "submit");
-    readButton.classList.add('read');
-    div.appendChild(deleteButton);
-    div.appendChild(readButton);
-    libraryGrid.appendChild(div);
-    count++;
+    libraryGrid.textContent = '';
+    for (let i = 0; i < 9; i++) {
+        let n = i + gridNum * 9;
+        if (n < myLibrary.length) {
+            const book = myLibrary[n];
+            let div = document.createElement('div');
+            div.classList.add('book');
+            div.setAttribute("data-key", n.toString());
+            div.textContent = book.info();
+            let deleteButton = document.createElement('button');
+            deleteButton.textContent = "Delete";
+            deleteButton.setAttribute("type", "submit");
+            deleteButton.classList.add('delete');
+            let readButton = document.createElement('button');
+            readButton.textContent = "Read?";
+            readButton.setAttribute("type", "submit");
+            readButton.classList.add('read');
+            div.appendChild(deleteButton);
+            div.appendChild(readButton);
+            libraryGrid.appendChild(div);
+        }
+    }
 }
 
 function addNewBookForm() {
@@ -48,16 +59,16 @@ function deleteBookFromLibrary(event) {
     if (event.target.classList.contains("delete")) {
         const bookElement = event.target.parentNode;
         const key = bookElement.getAttribute("data-key");
-        delete myLibrary[key];
-        bookElement.remove();
+        myLibrary.splice(key, 1);
+
+        generateLibraryGridForBoardNum(boardNumber);
     }
 }
 
 function toggleReadStatus(event) {
     if (event.target.classList.contains("read")) {
-        console.log("read");
         const bookElement = event.target.parentNode;
-        const key = bookElement.getAttribute("data-key");
+        const key = parseInt(bookElement.getAttribute("data-key"));
         let book = myLibrary[key];
         book.read = !(book.read);
         myLibrary[key] = book;
@@ -83,6 +94,22 @@ function submitBook(event) {
     addBookToLibrary(book);
 
 }
+
+function moveLeft() {
+    if (boardNumber > 0) {
+        boardNumber--;
+        generateLibraryGridForBoardNum(boardNumber);
+    }
+}
+
+function moveRight() {
+    const numBooks = myLibrary.length;
+    if (boardNumber < Math.floor( (numBooks - 1) / 9)) {
+        boardNumber++;
+        generateLibraryGridForBoardNum(boardNumber);
+    }
+}
+
 
 listenNewBookSubmit();
 listenDynamicEvents();
